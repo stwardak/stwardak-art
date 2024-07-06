7bff233688294b459e2d83dfffbd6d351d512f6f
stwardak <148924249+stwardak@users.noreply.github.com>
2024-07-06 17:17:22 -0500

    add show action for artworks

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,13 @@
 class ArtworksController < ApplicationController
+
   def index
     artworks = Artwork.all
     render json: artworks
   end
+
+  def show
+    artwork = Artwork.find(params[:id])
+    render json: artwork
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Artworks routes
   get "/artworks" => "artworks#index"
+  get "/artworks/:id" => "artworks#show"
+
   root 'artworks#index'
 
 end
