144503ac8d8011d7397a2e360985037a33f3de3e
stwardak <148924249+stwardak@users.noreply.github.com>
2024-07-18 21:45:05 -0500

    update collections index

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,13 +1,29 @@
 class CollectionsController < ApplicationController
   include Rails.application.routes.url_helpers
 
+  # def index
+  #   collections = Collection.includes(:artworks).all
+  #   collections_with_images = collections.map do |collection|
+  #     {
+  #       id: collection.id,
+  #       name: collection.name,
+  #       image_url: collection.artworks.first&.image&.attached? ? url_for(collection.artworks.first.image) : nil,
+  #       description: collection.description,
+  #       created_at: collection.created_at,
+  #       updated_at: collection.updated_at
+  #     }
+  #   end
+  #   render json: collections_with_images
+  # end
+
   def index
     collections = Collection.includes(:artworks).all
     collections_with_images = collections.map do |collection|
+      random_artwork = collection.artworks.sample
       {
         id: collection.id,
         name: collection.name,
-        image_url: collection.artworks.first&.image&.attached? ? url_for(collection.artworks.first.image) : nil,
+        image_url: random_artwork&.image&.attached? ? url_for(random_artwork.image) : nil,
         description: collection.description,
         created_at: collection.created_at,
         updated_at: collection.updated_at
