a46041d5b08c140f0e453e99a7837b63ba08bea1
stwardak <148924249+stwardak@users.noreply.github.com>
2024-07-19 12:03:59 -0500

    add users model and controller with test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :collections, only: [:index, :show]
     
     post 'contact', to: 'contacts#create'
+
+    resources :users, only: [:index, :show, :new, :create, :update, :destroy]
   
     root 'artworks#index'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_07_011636) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_19_165125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_07_011636) do
   create_table "collections", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
